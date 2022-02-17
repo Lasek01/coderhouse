cc882dbb24e7204febbb8654f323214d813e8105
Gabriel Da Silva <srgdsm98@gmail.com>
2022-02-16 23:47:27 -0300

    correccion de estilo de navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,18 +3,20 @@ import CartWidget from './CartWidget'
 
 const NavBar = () => {
     return (
-        <header className='nav-container'>
-            <div><h4>My React App</h4></div>
-            <div>
-                <nav className='nav-category-list'>
-                    <a href=''>Primer Link</a>
-                    <a href=''>Segundo Link</a>
-                    <a href=''>Tercer Link</a>
-                    <a href=''>Cuarto Link</a>
-                </nav>
-            </div>
-            <div className='nav-cart-icon'>
-                <CartWidget/>
+        <header className='header'>
+            <div className='nav-container'>
+                <div><h4>My React App</h4></div>
+                <div>
+                    <nav className='nav-category-list'>
+                        <a href=''>Primer Link</a>
+                        <a href=''>Segundo Link</a>
+                        <a href=''>Tercer Link</a>
+                        <a href=''>Cuarto Link</a>
+                    </nav>
+                </div>
+                <div className='nav-cart-icon'>
+                    <CartWidget/>
+                </div>
             </div>
         </header>
     )
